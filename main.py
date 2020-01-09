4edbe33f9be2d936fb056688aeafdfd3285be230
bartekcensorpic <56034779+bartekcensorpic@users.noreply.github.com>
2020-01-09 11:33:17 +0000

    csv file

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import argparse
-from src.utils import parse_args
-from src.experiment import prepare_experiment
+from src.args import process_arguments
+from src.experiment import Experiment
 
 
 if __name__ == '__main__':
-    args = parse_args()
+    args = process_arguments()
 
 
-
-
-    prepare_experiment(model_path=args.model_path,
+    exp = Experiment(model_path=args.model_path,
                        nudes_image_folder= args.nude_dataset_path,
                        non_nudes_image_folder= args.non_nude_dataset_path,
                        output_folder=args.output_path,
                        batch_size= args.batch_size,
                        image_size= (args.img_size, args.img_size),
                        classes= args.classes,
-                       image_extention = args.pictures_extention
+                       image_extention = args.pictures_extention,
                        )
+
+    exp.run_experiment()
 
--- src/experiment.py
+++ src/experiment.py
@@ -1,40 +1,89 @@
 import numpy as np
-
+import os
 from typing import Tuple, List
 from PIL import ImageFile
-
+import pandas as pd
 
 from src.model import load_model
 from src.images import get_images_from_folder, get_batches
+from src.utils import zip_prediction_with_classes, rounded_predictions
 
-def prepare_experiment(model_path:str,
-                       nudes_image_folder:str,
-                       non_nudes_image_folder:str,
-                       output_folder:str,
-                       batch_size:int,
-                       image_size:Tuple,
-                       classes:List,
-                       image_extention:str):
 
-    num_classes = len(classes)
-    tfmodel, input_details, output_detail =load_model(model_path,batch_size, image_size, num_classes)
+class Experiment:
+    def __init__(self, model_path: str,
+                 nudes_image_folder: str,
+                 non_nudes_image_folder: str,
+                 output_folder: str,
+                 batch_size: int,
+                 image_size: Tuple,
+                 classes: List,
+                 image_extention: str,
+                ):
+        self.model_path = model_path
+        self.nudes_image_folder = nudes_image_folder
+        self.non_nudes_image_folder = non_nudes_image_folder
+        self.output_folder = output_folder
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.classes = classes
+        self.image_extention = image_extention
 
-    list_nude_images_paths = get_images_from_folder(nudes_image_folder,image_extention)
-    list_non_nude_images_paths = get_images_from_folder(non_nudes_image_folder,image_extention)
 
-    results = run_experiment(tfmodel, input_details, output_detail,list_nude_images_paths, list_non_nude_images_paths, image_size, batch_size)
 
-def run_experiment(tf_model, input_details, output_details, nude_images_paths, non_nude_images_paths, image_size, batch_size):
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-    nude_batches = get_batches(nude_images_paths,batch_size, image_size)
-    non_nude_batches = get_batches(non_nude_images_paths, batch_size, image_size)
+    def __prepare(self):
+        num_classes = len(self.classes)
+        self.tf_model, self.input_details, self.output_details = load_model(self.model_path, self.batch_size, self.image_size, num_classes)
 
-    for batch in nude_batches:
-        tf_model.set_tensor(input_details[0]["index"], batch)
-        tf_model.invoke()
-        tflite_q_model_predictions = tf_model.get_tensor(output_details[0]["index"])
-        tflite_q_pred_idx = np.round(tflite_q_model_predictions)
+        self.list_nude_images_paths = get_images_from_folder(self.nudes_image_folder, self.image_extention)
+        self.list_non_nude_images_paths = get_images_from_folder(self.non_nudes_image_folder, self.image_extention)
+
+        self.df = pd.DataFrame(columns=['real_label']+self.classes)
+
+
+    def __log_predictions(self, predictions, real_label):
+
+        for i in range(predictions.shape[0]):
+            pred = predictions[i]
+            print(f"real label: {real_label}, "
+                  f"predictions: {zip_prediction_with_classes(pred,self.classes)}, "
+                  f"rounded_predictions: {rounded_predictions(pred, self.classes)}")
+            loc = len(self.df)
+            self.df.loc[loc] =  [real_label] + list(pred)
+
+    def __evaluate_batches(self, batches_gen, real_label):
+
+        for batch in batches_gen:
+            self.tf_model.set_tensor(self.input_details[0]["index"], batch)
+            self.tf_model.invoke()
+            tflite_q_model_predictions = self.tf_model.get_tensor(self.output_details[0]["index"])
+            tflite_q_pred_rounded = np.around(tflite_q_model_predictions, decimals=3)
+            self.__log_predictions(tflite_q_pred_rounded, real_label)
+
+
+    def run_experiment(self):
+        self.__prepare()
+
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+        nude_batches = get_batches(self.list_nude_images_paths, self.batch_size, self.image_size)
+        non_nude_batches = get_batches(self.list_non_nude_images_paths, self.batch_size, self.image_size)
+
+        self.__evaluate_batches(nude_batches, "nude")
+        self.__evaluate_batches(non_nude_batches, "non_nude")
+
+        save_file_path = os.path.join(self.output_details,'results.csv')
+        self.df.to_csv(save_file_path, index=False, quotechar='"', encoding='ascii')
+
+
+
+
+
+
+
+
+
+
 
 
 
